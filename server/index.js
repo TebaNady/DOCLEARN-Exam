6c3e4d5322700489965de78f36faa76cba46f9f4
TebaNady <teba7033@gmail.com>
2024-02-27 03:23:52 +0200

    Project modify

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const app = express();
 
 const port = 4000;
 
-app.use(cors()); //?app.use(cors({ origin: "*" })); allows all requests from anywhere to my server
+app.use(cors({
+    origin: ["*"],
+    methods: ["POST", "GET"],
+    credentials: true
+})); //?app.use(cors({ origin: "*" })); allows all requests from anywhere to my server
 //Start a connection to DB
 initConnection();
 
